ed614b302649cb04ad9d2e20475951169d26ed2c
ruslan <r_gimadeyev>
2021-02-16 22:21:59 +0600

    My first test)

---
+++ java-example/src/test/java/org/example/TestBase.java
@@ -0,0 +1,49 @@
+package org.example;
+
+import org.junit.After;
+import org.junit.Before;
+import org.openqa.selenium.By;
+import org.openqa.selenium.HasCapabilities;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+public class TestBase {
+
+    public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
+    public WebDriver driver;
+    public WebDriverWait wait;
+
+    @Before
+    public void start() {
+        if (tlDriver.get() != null) {
+            driver = tlDriver.get();
+            wait = new WebDriverWait(driver, 10);
+            return;
+        }
+        FirefoxOptions firefoxOptions = new FirefoxOptions();
+        firefoxOptions.setCapability("marionette", true);
+        driver = new FirefoxDriver(firefoxOptions);
+        tlDriver.set(driver);
+        System.out.println(((HasCapabilities) driver).getCapabilities());
+        wait = new WebDriverWait(driver, 10);
+
+        Runtime.getRuntime().addShutdownHook(
+                new Thread(() -> {
+                    driver.quit();
+                    driver = null;
+                }));
+    }
+
+    @After
+    public void stop() {
+        //driver.quit();
+        //driver = null;
+    }
+
+    void click(By locator) {
+        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(locator))).click();
+    }
+}
