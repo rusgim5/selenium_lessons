95d42204ed5de539b4601e1489d815b769617444
ruslan <r_gimadeyev>
2021-02-26 09:44:45 +0600

    Добавлен тест который прокликивает последовательно все пункты меню слева, включая вложенные пункты и проверяет наличие заголовка

--- java-example/src/test/java/org/example/MyFirstTest.java
+++ java-example/src/test/java/org/example/MyFirstTest.java
@@ -1,7 +1,9 @@
 package org.example;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 
 import static org.openqa.selenium.support.ui.ExpectedConditions.titleIs;
 
@@ -9,7 +11,7 @@ public class MyFirstTest extends TestBase {
 
     @Test
     public void myFirstTest() {
-        driver.navigate().to("http://www.google.com");
+        wd.navigate().to("http://www.google.com");
         type(By.name("q"), "webdriver");
         click(By.name("btnK"));
         wait.until(titleIs("webdriver - Поиск в Google"));
@@ -17,11 +19,28 @@ public class MyFirstTest extends TestBase {
 
     @Test
     public void testLogin() {
-        driver.navigate().to("http://localhost/litecart/admin");
-        type(By.name("username"), "admin");
-        type(By.name("password"), "admin");
-        click(By.name("login"));
+        login();
     }
 
+
+    @Test
+    public void testHeaders() {
+        login();
+//      В этом меню, прям любой клик пересоздает все элемены, из-за чего пришлось использовать for, а не foreach
+//      Пока не понял правильно ли так бороться с StaleElementReferenceException
+        if (isElementPresent(By.cssSelector("ul#box-apps-menu"))) {
+            int appMenuCount = wd.findElements(By.cssSelector("ul#box-apps-menu li#app-")).size();
+            for (int i = 1; i <= appMenuCount; i++) {
+                WebElement app = wd.findElement(By.cssSelector("ul#box-apps-menu li#app-:nth-child(" + i + ")"));
+                app.click();
+                int subDocCount = wd.findElements(By.cssSelector("li[id*=doc]")).size();
+                for (int j = 1; j <= subDocCount; j++) {
+                    WebElement doc = wd.findElement(By.cssSelector("li[id*=doc]:nth-child(" + j + ")"));
+                    doc.click();
+                    Assert.assertTrue("Header not found", isElementPresent(By.cssSelector("h1")));
+                }
+            }
+        }
+    }
 
 }
--- java-example/src/test/java/org/example/TestBase.java
+++ java-example/src/test/java/org/example/TestBase.java
@@ -4,37 +4,64 @@ import org.junit.After;
 import org.junit.Before;
 import org.openqa.selenium.By;
 import org.openqa.selenium.HasCapabilities;
+import org.openqa.selenium.TimeoutException;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import org.openqa.selenium.ie.InternetExplorerOptions;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
+import static org.example.TestBase.BrowserType.*;
 
 public class TestBase {
 
     public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<>();
-    public WebDriver driver;
+    public WebDriver wd;
     public WebDriverWait wait;
+    BrowserType browserType;
 
     @Before
     public void start() {
         if (tlDriver.get() != null) {
-            driver = tlDriver.get();
-            wait = new WebDriverWait(driver, 10);
+            wd = tlDriver.get();
+            wait = new WebDriverWait(wd, 10);
             return;
         }
-        FirefoxOptions firefoxOptions = new FirefoxOptions();
-        firefoxOptions.setCapability("marionette", true);
-        driver = new FirefoxDriver(firefoxOptions);
-        tlDriver.set(driver);
-        System.out.println(((HasCapabilities) driver).getCapabilities());
-        wait = new WebDriverWait(driver, 10);
+        browserType = FIREFOX;
 
-        Runtime.getRuntime().addShutdownHook(
-                new Thread(() -> {
-                    driver.quit();
-                    driver = null;
-                }));
+        if (browserType.equals(CHROME)) {
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.setCapability("marionette", true);
+            wd = new ChromeDriver(chromeOptions);
+        } else if (browserType.equals(FIREFOX)) {
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.setCapability("marionette", true);
+            wd = new FirefoxDriver(firefoxOptions);
+        } else if (browserType.equals(IE)) {
+            InternetExplorerOptions internetExplorerOptions = new InternetExplorerOptions();
+            internetExplorerOptions.setCapability("marionette", true);
+//            internetExplorerOptions.setCapability("logLevel", "DEBUG");
+            wd = new InternetExplorerDriver(internetExplorerOptions);
+        }else if (browserType.equals(FIREFOX_NIGHTLY)) {
+            FirefoxOptions firefoxOptions = new FirefoxOptions();
+            firefoxOptions.setBinary("C:/Program Files/Firefox Nightly/firefox.exe");
+            firefoxOptions.setCapability("marionette", true);
+            wd = new FirefoxDriver(firefoxOptions);
+        }
+
+        tlDriver.set(wd);
+        System.out.println(((HasCapabilities) wd).getCapabilities());
+        wait = new WebDriverWait(wd, 10);
+
+//        Runtime.getRuntime().addShutdownHook(
+//                new Thread(() -> {
+//                    driver.quit();
+//                    driver = null;
+//                }));
     }
 
     @After
@@ -44,11 +71,36 @@ public class TestBase {
     }
 
     void click(By locator) {
-        wait.until(ExpectedConditions.elementToBeClickable(driver.findElement(locator))).click();
+        wait.until(ExpectedConditions.elementToBeClickable(wd.findElement(locator))).click();
     }
+
     void type(By locator, String value) {
-        driver.findElement(locator).click();
-        driver.findElement(locator).clear();
-        driver.findElement(locator).sendKeys(value);
+        wd.findElement(locator).click();
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(value);
     }
+
+    protected Boolean isElementPresent(By locator) {
+        try {
+            wait.until((WebDriver wd) -> wd.findElement(locator));
+            return true;
+        } catch (TimeoutException ex) {
+            return false;
+        }
+    }
+
+    enum BrowserType {
+        CHROME,
+        FIREFOX,
+        IE,
+        FIREFOX_NIGHTLY,
+    }
+
+    protected void login() {
+        wd.navigate().to("http://localhost/litecart/admin");
+        type(By.name("username"), "admin");
+        type(By.name("password"), "admin");
+        click(By.name("login"));
+    }
+
 }
